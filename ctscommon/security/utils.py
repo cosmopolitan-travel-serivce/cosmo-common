755a636a472473d3bf94b11ca774f6c13bf05a06
hannmbaye <mbaye@ctsfares.com>
2019-09-19 16:50:51 +0000

    udate  get acces token and common version

--- ctscommon/security/utils.py
+++ ctscommon/security/utils.py
@@ -15,7 +15,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = get_config("JWT_SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
 ALGORITHM = get_config("JWT_ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = get_config("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+ACCESS_TOKEN_EXPIRE_MINUTES = int(get_config("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
 def verify_password(plain_password, hashed_password):
