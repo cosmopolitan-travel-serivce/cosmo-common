e0cb3035bdea9da33ff6e98ef8134b9ca9ac1c8a
hannmbaye <mbaye@ctsfares.com>
2019-09-30 10:21:14 +0000

    add ctsstaff and cst admin to CTSUSER

--- ctscommon/security/utils.py
+++ ctscommon/security/utils.py
@@ -52,11 +52,13 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> CTSUser:
         managed_customers: List[str] = payload.get("managed_customers")
         customer: str = payload.get("customer")
         offices: List[str] = payload.get("offices")
+        is_cts_staff: bool = payload.get("is_cts_staff")
+        is_agency_admin: bool = payload.get("is_agency_admin")
         # expiration_date: str = payload.get("exp")
         if username is None:
             raise credentials_exception
         user = CTSUser(username=username, email=email, full_name=None, customer=customer, permissions=permissions,
-                       managed_customers=managed_customers, offices=offices)
+                       managed_customers=managed_customers, offices=offices, is_cts_staff=is_cts_staff, is_agency_admin=is_agency_admin)
     except PyJWTError:
         raise credentials_exception
     if user is None:
