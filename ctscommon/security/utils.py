51a28640c975861f4a89c500aa5c73c00e941aa9
Mouhamad THIAM <mohamed@ctsfares.com>
2019-10-07 18:38:02 +0000

    flake satisfy

--- ctscommon/security/utils.py
+++ ctscommon/security/utils.py
@@ -8,12 +8,13 @@ from jwt import PyJWTError
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from ctscommon.config.loader import get_config
-from ctscommon.security import oauth2_scheme, CTSUser
+from ctscommon.security import oauth2_scheme
+from ctscommon.security.models import CTSUser
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
-SECRET_KEY = get_config("JWT_SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
+default_jwt_secret_key = None
+SECRET_KEY = get_config("JWT_SECRET_KEY", default_jwt_secret_key)
 ALGORITHM = get_config("JWT_ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = get_config("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 30)
 
@@ -51,8 +52,8 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> CTSUser:
         customer: str = payload.get("customer")
         if username is None:
             raise credentials_exception
-        user = CTSUser(username=username, email=email, full_name=None, customer=customer, permissions=permissions,
-                       managed_customers=managed_customers)
+        user = CTSUser(username=username, email=email, full_name=None, customer=customer,
+                       permissions=permissions, managed_customers=managed_customers)
     except PyJWTError:
         raise credentials_exception
     if user is None:
