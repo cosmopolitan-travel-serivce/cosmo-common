be5cc5d05c91fbc3f663b227560d696c927b0a8a
zeynabsaly <zeynab@ctsfares.com>
2019-10-14 13:18:16 +0000

    updated model reservation

--- ctscommon/clients/portal/reservation_client/routes_reservation.py
+++ ctscommon/clients/portal/reservation_client/routes_reservation.py
@@ -1,8 +1,14 @@
-from ctscommon.clients import MicroServiceClient
-from ctscommon.clients.portal.reservation.models import DisplayPnr, PassengerType, SearchPriceQuote, DetailDeals, DetailPassengers, CustomerIdentifier, CreatePnr
-from typing import List
+from fastapi import APIRouter
+from portal.reservation.reservation_controller import ReservationController
+from portal.reservation.reservation_schema import SearchPriceQuote, CreatePnr
+from portal.reservation.reservation_utils import get_region_name
+from starlette.responses import Response
 
 
 class ReservationClient(MicroServiceClient):
     def __init__(self):
         MicroServiceClient.__init__(self, "PORTAL", "/api/reservation")
+        
+    def get_reservation(self, pnr: str, message_id: str = None, close_session: bool = True):
+        return self._get_url("/")
+
