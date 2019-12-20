593678a8c3a33102b2e3a697cdb44cbfdbcbd2e8
Demba <demba@ctsfares.com>
2019-12-20 16:38:18 +0000

    auth url for client

--- ctscommon/security/__init__.py
+++ ctscommon/security/__init__.py
@@ -1,3 +1,10 @@
 from fastapi.security import OAuth2PasswordBearer
+from ctscommon.config.loader import get_config
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token")
+AUTH_URL = get_config("AUTH_URL", None)
+TOKEN_URL = "/api/auth/token"
+if AUTH_URL:
+    TOKEN_URL = AUTH_URL + TOKEN_URL
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=TOKEN_URL)
+optional_oauth2_scheme = OAuth2PasswordBearer(tokenUrl=TOKEN_URL, auto_error=False)
