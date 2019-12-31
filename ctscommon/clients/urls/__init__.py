0b6d9269b4339e528acfb2a9244f60b9088fa7e1
Bamba4 <diagnec341@gmail.com>
2019-12-31 03:53:31 -0800

    comment on __init__ files

--- ctscommon/clients/urls/__init__.py
+++ ctscommon/clients/urls/__init__.py
@@ -1,11 +1,13 @@
 from typing import List
+
 from ctscommon.clients.alternate_worker import ApiResponseError
 from ctscommon.urls.schemas import ApiUrl, ApiUrlUpdate, ApiUrlCreate, HttpMethod
+from ctscommon.clients import MicroServiceClient
 
 
 class ApiUrlClient(MicroServiceClient):
     """
-     This client will handle everything about API urls
+    This client will handle everything about API urls
     """
     def __init__(self):
         MicroServiceClient.__init__(self, "AUTH", "api/security/urls")
@@ -25,10 +27,10 @@ class ApiUrlClient(MicroServiceClient):
 
     def batch_create_service_urls(self, service: str, api_urls: List[ApiUrlCreate]) -> List[ApiUrl]:
         """
-            Create on the fly many API urls
-            :param service: str -> The service to which urls belong
-            :param api_urls: List[ApiUrlCreate] -> List of information about api urls
-            :return: List[ApiUrl] -> Created API urls
+        Create on the fly many API urls
+        :param service: str -> The service to which urls belong
+        :param api_urls: List[ApiUrlCreate] -> List of information about api urls
+        :return: List[ApiUrl] -> Created API urls
         """
         data = [api_url.dict(skip_defaults=True) for api_url in api_urls]
         response = self._post_url(f"/{service}/batch-create", data)
@@ -39,9 +41,9 @@ class ApiUrlClient(MicroServiceClient):
 
     def get_service_urls(self, service: str) -> List[ApiUrl]:
         """
-            Get all API urls belonging to a given service
-            :param service: str -> The service
-            :return: List[ApiUrl] -> All API urls belonging to that service
+        Get all API urls belonging to a given service
+        :param service: str -> The service
+        :return: List[ApiUrl] -> All API urls belonging to that service
         """
         response = self._get_url(f"/{service}")
         if response.status_code == 200:
@@ -51,8 +53,8 @@ class ApiUrlClient(MicroServiceClient):
 
     def get_all_urls(self) -> List[ApiUrl]:
         """
-            Get all API urls whatever their services
-            :return: List[ApiUrl] -> List of API urls
+        Get all API urls whatever their services
+        :return: List[ApiUrl] -> List of API urls
         """
         response = self._get_url("/")
         if response.status_code == 200:
@@ -62,12 +64,12 @@ class ApiUrlClient(MicroServiceClient):
 
     def update_service_url(self, service: str, operation_id: str, method: HttpMethod, url_update: ApiUrlUpdate) -> ApiUrl:
         """
-            Update an API url infos
-            :param service: str -> The service
-            :param operation_id: str -> The operation id identifying the action
-            :param method: HttpMethod -> the HTTP method corresponding to the action
-            :param url_update: ApiUrlUpdate -> The update infos
-            :return: ApiUrl -> The updated API url
+        Update an API url infos
+        :param service: str -> The service
+        :param operation_id: str -> The operation id identifying the action
+        :param method: HttpMethod -> the HTTP method corresponding to the action
+        :param url_update: ApiUrlUpdate -> The update infos
+        :return: ApiUrl -> The updated API url
         """
         response = self._put_url(f"/{service}/{operation_id}/{method}", url_update.dict(skip_defaults=True))
         if response.status_code == 202:
@@ -77,12 +79,12 @@ class ApiUrlClient(MicroServiceClient):
 
     def get_service_url(self, service: str, operation_id: str, method: HttpMethod) -> ApiUrl:
         """
-         Get an API url
-         :param service: str -> The service
-         :param operation_id: str -> The operation id identifying the action
-         :param method: HttpMethod -> the HTTP method corresponding to the action
-         :return: ApiUrl -> The API url
-         """
+        Get an API url
+        :param service: str -> The service
+        :param operation_id: str -> The operation id identifying the action
+        :param method: HttpMethod -> the HTTP method corresponding to the action
+        :return: ApiUrl -> The API url
+        """
         response = self._get_url(f"/{service}/{operation_id}/{method}")
         if response.status_code == 200:
             return ApiUrl(**response.payload)
@@ -91,16 +93,14 @@ class ApiUrlClient(MicroServiceClient):
 
     def delete_service_url(self, service: str, operation_id: str, method: HttpMethod) -> bool:
         """
-            Delete an API url
-            :param service: str -> The service
-            :param operation_id: str -> The operation id identifying the action
-            :param method: HttpMethod -> the HTTP method corresponding to the action
-            :return: bool -> To tell if it is deleted or not
+        Delete an API url
+        :param service: str -> The service
+        :param operation_id: str -> The operation id identifying the action
+        :param method: HttpMethod -> the HTTP method corresponding to the action
+        :return: bool -> To tell if it is deleted or not
         """
         response = self._delete_url(f"/{service}/{operation_id}/{method}", None)
         if response.status_code == 202:
             return True
         else:
             raise ApiResponseError(response.status_code, response.payload)
-
-
